8583269581007106b1780af39d2871c3cb341478
nhernandez2020@hotmail.com <Nhernandez2020@hotmail.com>
2020-04-17 21:46:23 -0700

    fixed app now working and deleted un needed files

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,21 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCQ-cBA316wDMdVkGKYl22dlP8d7iVdK-g",
+    authDomain: "train-db66b.firebaseapp.com",
+    databaseURL: "https://train-db66b.firebaseio.com",
+    projectId: "train-db66b",
+    storageBucket: "train-db66b.appspot.com",
+    messagingSenderId: "741844955892",
+    appId: "1:741844955892:web:f95d2206186215430064a1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+console.log(firebaseConfig, firebase)
+
+// setting database to a variable
+var db = firebase.database().ref();
+
+
 window.onload = function () {
     var frequency;
     var minutesAway;
@@ -7,31 +25,19 @@ window.onload = function () {
     var dest;
     var counter = 0;
 
-    // Your web app's Firebase configuration
-    var firebaseConfig = {
-            apiKey: "AIzaSyCSRRh1c8X7zwZaW-zCq6Hi_wMKBtYSyOk",
-            authDomain: "train-scheduler-fa9f2.firebaseapp.com",
-            databaseURL: "https://train-scheduler-fa9f2.firebaseio.com",
-            projectId: "train-scheduler-fa9f2",
-            storageBucket: "train-scheduler-fa9f2.appspot.com",
-            messagingSenderId: "313125405669",
-            appId: "1:313125405669:web:cdeaa1a035f3b79e25a50c",
-            measurementId: "G-0R2G2NDJHE"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
 
-    // setting database to a variable
-    const db = firebase.database().ref();
+  
 
     // grabbing variables from database
-    db.on("value", function (snapshot) {
+    db.on("child_added", function (snapshot) {
         //using an if statement to stop initial submit from posting twice
-        if (counter < 1) {
+        console.log(snapshot.val())
+        let train=calculateTrain(snapshot.val().first)
+        // if (counter < 1) {
             //appending variables from database
-            $("#body").append("<tr><td>" + snapshot.val().name + "</td><td>" + snapshot.val().dest + "</td><td>" + snapshot.val().frequency + "</td><td>" + snapshot.val().nextArrival + "</td><td>" + snapshot.val().minutesAway + "</td></tr>");
-            counter += 1;
-        }
+            $("#body").append("<tr><td>" + snapshot.val().name + "</td><td>" + snapshot.val().dest + "</td><td>" + snapshot.val().frequency + "</td><td>" + train.nextArrival + "</td><td>" + train.minutesAway + "</td></tr>");
+
+        // }
     })
 
     // when clicking submit button
@@ -46,21 +52,32 @@ window.onload = function () {
         time();
 
         //sending variables to database
-        db.set({
+        db.push({
             name: name,
             dest: dest,
             frequency: frequency,
-            nextArrival: nextArrival,
-            minutesAway: minutesAway
+          
+           firstArival : first
         });
+        console.log({
+            name: name,
+            dest: dest,
+            frequency: frequency,
 
+            firstArival: first
+        })
+        $("#trainName").val("")
+        $("#destination").val("")
+        $("#first").val("")
+        $("#fequency").val("")
+    
         //adding new row to table
-        $("#body").append("<tr><td>" + name + "</td><td>" + dest + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>")
+        // $("#body").append("<tr><td>" + name + "</td><td>" + dest + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>")
 
     });
 
     // function to calculate nextArrival & minutesAway
-    function time() {
+    function calculateTrain(first) {
         // First Time (pushed back 1 year to make sure it comes before current time)
         var firstTimeConverted = moment(first, "HH:mm").subtract(1, "years");
 
@@ -76,6 +93,7 @@ window.onload = function () {
         // Next Train
         var nextTrain = moment().add(minutesAway, "minutes");
         nextArrival = moment(nextTrain).format("hh:mm");
+        return({nextArrival:nextArrival,minutesAway:minutesAway})
     };
 
 };
